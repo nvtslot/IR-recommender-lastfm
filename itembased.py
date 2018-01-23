05f7cf44474d2b0c1f4997acf9d6de135b6ea75c
wesselreijngoud <wesselreijn@live.nl>
2018-01-23 15:29:46 +0100

    Add files via upload
    Current output for user 2 and 3 :
    
    2 	 [('2936', 0.8695330139434974), ('85', 0.8602555558643917), ('2559', 0.8457668049336197), ('2552', 0.8410633911592964), ('999', 0.8308793183714108), ('91', 0.8279779952073367), ('51', 0.8241189371181398), ('1013', 0.8237219700330841), ('997', 0.8209942581263279), ('193', 0.8201701027149504), ('1895', 0.8123174496268525), ('1892', 0.8027657348591467), ('3768', 0.8027540219240415), ('61', 0.8004124510450292), ('2937', 0.7974202444927063), ('994', 0.7914672795166294), ('1006', 0.7901744265512675), ('998', 0.7895467606020499), ('4977', 0.7895420339517227), ('1008', 0.7887608503885728)]
    3 	 [('9336', 0.9019752336033946), ('10194', 0.8660254037844387), ('6761', 0.8429272304235246), ('14560', 0.8249579113843055), ('9451', 0.8164965809277261), ('10382', 0.8164965809277261), ('10726', 0.8164965809277261), ('6169', 0.816496580927726), ('17380', 0.7777777777777778), ('6173', 0.7745966692414834), ('6757', 0.769800358919501), ('7895', 0.7492686492653552), ('14869', 0.7453559924999299), ('12703', 0.7396002616336388), ('14194', 0.7302967433402214), ('4230', 0.7229568912920512), ('162', 0.7196715715299218), ('1731', 0.7169771439171534), ('1423', 0.710046946804693), ('10719', 0.7071067811865476)]

---
+++ itembased.py
@@ -0,0 +1,75 @@
+import sys
+import operator
+import linecache
+import pickle
+
+# open files
+similaritems = pickle.load(open("item_sim01", "rb"))
+#listens = open('user_artists.dat', 'r')
+itemlisten = pickle.load(open("itemlisten", "rb"))
+testfile = open("test.txt", "r")
+
+def indexFile(similaritems):
+	"""indexes the file so that it is faster afterwards"""
+	nr = 1
+	index = {}
+	for line in similaritems:
+		BAND1 = line.split(' ')[0]
+		if (BAND1 not in index):
+			index[BAND1] = [nr]
+		else:
+			index[BAND1].append(nr)
+		nr += 1
+
+	return index
+
+"""
+#####FUNCTION NOT NEEDED ANYMORE AS IT IS NOW INCLUDED IN A PICKLE BUT PLEASE LEAVE HERE FOR CREATION OF NEW PICKLE.
+def alreadyListens(listens):
+	#creates a dict of key:value where key=user value=artist to show what artists user listens to already
+	listendict = {}
+	for line in listens.readlines()[1:]:
+		
+		user, artist, count = line.strip().split("\t")
+		if user in listendict:
+			listendict[user].append(artist)
+		else:
+			listendict[user] = [artist]
+	pickle.dump(listendict, open("itemlisten", "wb"))
+	#print(listendict)
+	return listendict
+
+
+"""
+
+def test(itemlisten,index,n):
+	similarities = {}
+	similarband = {}
+	for line in testfile:
+		user, band, count = line.rstrip().split('\t')
+		listensalready = itemlisten[user]
+		for band in listensalready:
+			try:
+				similarband = similaritems[band]
+				topNsimilarBands = sorted(similarband.items(),key=operator.itemgetter(1), reverse=True)[:n]
+				
+				
+			except KeyError:
+				pass
+		
+		
+		print(user, '\t', topNsimilarBands)
+	
+		
+
+
+
+
+def main():
+    index = indexFile(similaritems)
+    #listendict = alreadyListens()
+    doeiets = test(itemlisten,index, 20)
+
+
+if __name__ == "__main__":
+    main()
